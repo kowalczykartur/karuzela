8fa462ad9f72211904c4076d56d34b307d687d38
kowalczykartur <kowalczyk_artur@interia.eu>
2017-11-20 21:34:08 +0100

    jest karuzela

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,58 @@
-$("span:even").css('color', 'red');
-var paragraphs = $('p');
-paragraphs.each(function(index, element) {
-
-    var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
-    $(element).append(button)
-
+$(function(){
+     var slideIndex = 1;
+	var carouselList = $("#carousel ul");
+    setInterval(changeSlide, 3000);
+    function changeSlide(slideNumber) {
+        if (slideNumber){
+            carouselList.animate({'marginLeft':0}, 100, moveFirstSlide);
+            carouselList.animate({'marginLeft':(-400*slideNumber)}, 100, moveFirstSlide);
+            slideIndex=slideNumber;
+        }
+        else{
+            carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+             console.log ('slideIndex', slideIndex);
+        
+        slideIndex ++;
+        
+        if (slideIndex > 5){
+            slideIndex = 1;
+        }
+        }
+       
+        activeDot(slideIndex);
+        
+    }
+    function moveFirstSlide () {
+        var firstItem = carouselList.find("li:first");
+        var lastItem = carouselList.find("li:last");
+        lastItem.after(firstItem);
+        carouselList.css({marginLeft:0});
+    }
+    function moveLeft(){
+        carouselList.animate({'marginLeft':+400}, 500, moveFirstSlide);
+        slideIndex --;
+        if (slideIndex < 1){
+            slideIndex = 5;
+            }
+        activeDot(slideIndex);
+        }
+    $(".arrow-left").on("click", function(event) {
+	moveLeft();
 });
-$("button").click(function(){
-	alert($(this).attr("data-tmp"));
+    
+    $(".arrow-right").on("click", function(event) {
+	changeSlide();
 });
+   
+   function activeDot (slideIndex){
+       $("#dots i ").removeClass("active");
+       $("#dots i:nth-child("+(slideIndex) +")").addClass("active");
+   }
+    $("#dots i").on("click", function(event) {
+    var clickedDot = $(this).index() +1;
+    console.log('Klikniete', clickedDot);
+        changeSlide(clickedDot);
+});
+    
+});
+
